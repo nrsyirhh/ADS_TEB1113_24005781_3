2307195e9e827bf2cf3323c4c1a0759c5e4fa3b1
insyi ⋅˚₊‧ ୨୧ ‧₊˚ ⋅ <syiisyirah@outlook.com>
2026-06-27 23:17:37 +0800

    updated lab5-task 3

--- 2026-06-26-Lab-5/Lab5-Task3.cpp
+++ 2026-06-26-Lab-5/Lab5-Task3.cpp
@@ -1,68 +1,100 @@
 #include <iostream>
 #include <string>
 using namespace std;
-// QUEUE - LIFO
 
-class Node{
-    public:
+// QUEUE - FIFO (First In First Out)
+class Node {
+public:
     string name;
     Node* next;
 
-    Node(string value){
-        name = value 
+    Node(string value) {
+        name = value;
         next = nullptr;
     }
 };
 
-class Queue{
-    public:
+class Queue {
+public:
     Node* front;
     Node* rear;
 
-    Queue(){
+    Queue() {
         front = nullptr;
         rear = nullptr;
     }
 
-    void enqueue(string value){
+    void enqueue(string value) {
         Node* newNode = new Node(value);
 
-        // If the line is completely empty
-        if (rear==nullptr){
+        // if queue is empty
+        if (rear == nullptr) {
             front = newNode;
             rear = newNode;
             return;
         }
-        // If people are already in line
-        rear -> next = newNode;
+        // if there is a person ....
+        rear->next = newNode;
         rear = newNode;
     }
 
-    void dequeue(){
-        //if line is empty
-        if (front == nullptr){
+    void dequeue() {
+        // if queue is emptry
+        if (front == nullptr) {
             cout << "Queue is empty! Nobody in line." << endl;
             return;
         }
-    Node* current = front; // Grab the person currently at the front
-    front = front -> next; // Make the 2nd person the new front of the line
-    }
+        
+        Node* current = front; // Take person in the front
+        front = front->next;   // Second person become first
 
-    if (front == nullptr) {
+        
+        if (front == nullptr) {
             rear = nullptr;
         }
 
-        delete current
-}
+        delete current; 
+    }
 
-// peek
-
-string peek() {
+    // peek
+    string peek() {
         if (front == nullptr) {
             return "Queue is empty";
         }
         return front->name;
     }
 
-// display
+    void display() {
+        if (front == nullptr) {
+            cout << "Queue is empty!" << endl;
+            return;
+        }
+        // temp = temporary
+        Node* temp = front;
+        cout << "Queue: ";
+        while (temp != nullptr) {
+            cout << temp->name << " -> ";
+            temp = temp->next;
+        }
+        cout << "" << endl;
+    }
+};
 
+int main() {
+    Queue q;
+    
+    q.enqueue("Aimar");
+    q.enqueue("Anjana");
+    q.enqueue("Ahmad");
+    
+    q.display();
+    
+    cout << "Front of queue (peek): " << q.peek() << endl;
+    
+    cout << "Dequeueing 1 person..." << endl;
+    q.dequeue();
+    
+    q.display();
+    
+    return 0;
+}
